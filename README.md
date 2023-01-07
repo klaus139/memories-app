40352bb06ee4e0db47ac3e5c1838d0516fcc640c
Nicholas igunbor <nickoklaus5@gmail.com>
2023-01-07 11:17:42 +0100

    initialized a project

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a memories Application. I will update the readme as i build the app
---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL="mongodb+srv://klaus139:klaus139@memoriesproject.k0h5oxg.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, () => console.log(`database connected && Server running on port ${PORT}`)))
+.catch((error)=> console.log(error.message)) 
+
+
