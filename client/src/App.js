84fc91d55e4fcb12ff39b5042475d12f9161500a
Nicholas igunbor <nickoklaus5@gmail.com>
2023-01-07 13:05:09 +0100

    let the crazy begin

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import memories from './images/memories.png'
+
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar posiiton='static' color='inherit'>
+                <Typography variant='h2' align='center'>Memories</Typography>
+                <img src={memories} alt='memories' height='300' />
+                
+            </AppBar>
+        </Container>
     )
 }
 
